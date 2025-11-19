c460b2fe98c3221e338fbf3b91cee93feed3e303
MagicalMatrix <magical0matrix@gmail.com>
2025-11-20 00:34:34 +0100

    button connection with UI change

--- IOS_Prog_2025/ViewController.swift
+++ IOS_Prog_2025/ViewController.swift
@@ -9,11 +9,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CalcBufer: UILabel!
+    
+    @IBOutlet weak var CalcResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func ClearAll(_ sender: Any) {
+    }
+    
+    @IBAction func Confirm(_ sender: Any) {
+    }
+    
+    @IBAction func Addition(_ sender: Any) {
+    }
+    
+    @IBAction func Subtraction(_ sender: Any) {
+    }
+    
+    @IBAction func Multiplication(_ sender: Any) {
+    }
+    
+    @IBAction func Division(_ sender: Any) {
+    }
+    
+    @IBAction func Percentage(_ sender: Any) {
+    }
+    
+    @IBAction func Logarithm(_ sender: Any) {
+    }
+    
+    @IBAction func Power(_ sender: Any) {
+    }
+    
+    //-------------------------------------------------------------
+    
+    
+    @IBAction func Zero(_ sender: Any) {
+    }
+    
+    @IBAction func One(_ sender: Any) {
+    }
+    
+    @IBAction func Two(_ sender: Any) {
+    }
+    
+    @IBAction func Three(_ sender: Any) {
+    }
+    
+    @IBAction func Four(_ sender: Any) {
+    }
+    
+    @IBAction func Five(_ sender: Any) {
+    }
+    
+    @IBAction func Six(_ sender: Any) {
+    }
+    
+    @IBAction func Seven(_ sender: Any) {
+    }
+    
+    @IBAction func Eight(_ sender: Any) {
+    }
+    
+    @IBAction func Nine(_ sender: Any) {
+    }
 }
 
